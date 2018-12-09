d88d5c9d84c82fff3275490fdc9a33dfdbbc5082
Rohan Gupta <39908472+DemonDaddy22@users.noreply.github.com>
2018-12-09 10:23:26 +0530

    Create campground.js

---
+++ models/campground.js
@@ -0,0 +1,38 @@
+var mongoose = require("mongoose");
+
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    imageId: String,
+    price: String,
+    location: String,
+    lat: Number,
+    lng: Number,
+    description: String,
+    createdAt: { type: Date, default: Date.now },
+    author: {
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        username: String
+    },
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Comment"
+        }
+    ],
+    reviews: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Review"
+        }
+    ],
+    rating: {
+        type: Number,
+        default: 0
+    }
+});
+
+module.exports = mongoose.model("Campground", campgroundSchema);
